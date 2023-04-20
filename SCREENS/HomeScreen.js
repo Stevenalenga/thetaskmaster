e52cf1054d74c1cc08b96c172f2f94e5248ba0bc
Stevenalenga <steven.alenga@gmail.com>
2023-04-20 17:02:02 +0300

    initial commit

--- SCREENS/HomeScreen.js
+++ SCREENS/HomeScreen.js
@@ -1,10 +1,45 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
 
 function HomeScreen() {
   return (
     <View>
-      <Text>Home Screen</Text>
+      <Text>HomeScreen</Text>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = "ios-home";
+            } else if (route.name === "Profile") {
+              iconName = "ios-person";
+            } else if (route.name === "Notifications") {
+              iconName = "ios-notifications";
+            } else if (route.name === "Search") {
+              iconName = "ios-search";
+            } else if (route.name === "Messages") {
+              iconName = "ios-mail";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Notifications" component={NotificationsScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Messages" component={MessagesScreen} />
+      </Tab.Navigator>
     </View>
   );
 }
--- test2.js
+++ test2.js
@@ -3,8 +3,11 @@ import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
 const Tab = createMaterialTopTabNavigator();
 
 function MyStack() {
@@ -30,25 +33,32 @@ function ProfileTabNavigator() {
   );
 }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
-  );
-}
-
 function LoginScreen() {
   const navigation = useNavigation();
 
   return (
-    <View>
-      <Text>Login Screen</Text>
-      <TextInput placeholder="Email" />
-      <TextInput placeholder="Password" secureTextEntry={true} />
-      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-        <Text>Register</Text>
-      </TouchableOpacity>
+    <View style={styles.parentContainer}>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <TextInput placeholder="Username" style={styles.input} />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry
+            style={styles.input}
+          />
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+          <Text style={styles.register}>Register</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -64,7 +74,38 @@ function RegisterScreen() {
 function HomeScreen() {
   return (
     <View>
-      <Text>Home Screen</Text>
+      <Text>HomeScreen</Text>
+      <Tabs.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = "ios-home";
+            } else if (route.name === "Profile") {
+              iconName = "ios-person";
+            } else if (route.name === "Notifications") {
+              iconName = "ios-notifications";
+            } else if (route.name === "Search") {
+              iconName = "ios-search";
+            } else if (route.name === "Messages") {
+              iconName = "ios-mail";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tabs.Screen name="Home" component={HomeScreen} />
+        <Tabs.Screen name="Profile" component={ProfileScreen} />
+        <Tabs.Screen name="Notifications" component={NotificationsScreen} />
+        <Tabs.Screen name="Search" component={SearchScreen} />
+        <Tabs.Screen name="Messages" component={MessagesScreen} />
+      </Tabs.Navigator>
     </View>
   );
 }
